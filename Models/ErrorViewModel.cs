93d1ef442f2eb017b4e28ce03d437404a028732b
yusfa julian <yusfajulian44@gmail.com>
2022-01-30 03:18:25 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Travelingyu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
